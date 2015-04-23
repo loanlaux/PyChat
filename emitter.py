1bd00364bb48e0ac33163cb6f572051b098d915a
Loan Laux <spam@loanlaux.fr>
2015-04-23 19:19:59 +0200

    Bug fixes & started to implement JSON-based network communication frames

--- emitter.py
+++ emitter.py
@@ -4,6 +4,14 @@ import socket
 import time
 import sys
 import signal
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", help = "increases command line verbosity", action = "store_true")
+parser.add_argument("port", help = "network communication port", type = int)
+parser.add_argument("username", help = "chat username to use", type = str)
+args = parser.parse_args()
 
 def signalHandler(signal, frame):
     print("\n\nClosing socket...")
@@ -13,24 +21,22 @@ def signalHandler(signal, frame):
 
 signal.signal(signal.SIGINT, signalHandler)
 
-try:
-    port = int(sys.argv[1])
-
-except:
-    print("Usage: " + sys.argv[0] + " [port]")
-    sys.exit(0)
-
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-connection.bind(('', port))
+connection.bind(('', args.port))
 connection.listen(5)
 
 clientConnection, connectionInfo = connection.accept()
 
-print("Ready to send on port " + str(port))
+print("Ready to send on port " + str(args.port))
 
 while True:
     message = str(input("Message: "))
-    clientConnection.send(message.encode("utf-8"))
+    messageObject = json.dumps({ u"username": args.username,
+        u"time": time.time(),
+        u"message": message
+    })
+
+    clientConnection.send(messageObject.encode('utf-8'))
 
 signal.pause()
--- receiver.py
+++ receiver.py
@@ -3,6 +3,16 @@
 import socket
 import sys
 import signal
+import argparse
+import json
+from datetime import datetime
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", help = "increases command line verbosity", action = "store_true")
+parser.add_argument("host", help = "server IP address", type = str)
+parser.add_argument("port", help = "network communication port to use", type = int)
+parser.add_argument("username", help = "chat username to use", type = str)
+args = parser.parse_args()
 
 def signalHandler(signal, frame):
     print("\n\nClosing socket...")
@@ -12,22 +22,14 @@ def signalHandler(signal, frame):
 
 signal.signal(signal.SIGINT, signalHandler)
 
-try:
-    port = int(sys.argv[1])
-    host = str(sys.argv[2])
-
-except:
-    print("Usage: " + sys.argv[0] + " [port] [host]")
-    sys.exit(0)
-
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect((host, port))
+connection.connect((args.host, args.port))
 
 while True:
     message = connection.recv(1024)
 
     if message:
         message = message.decode("utf-8")
-        print(message)
+        print(json.loads(message)['username'] + " - " + datetime.fromtimestamp(json.loads(message)['time']).strftime('%H:%M') + " > " + json.loads(message)['message'])
 
 signal.pause()
